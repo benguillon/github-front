991ffec2a19340d07773a6f709faf29e29f7d03a
benjaminguillon <benjamin.guillon@epsi.fr>
2019-04-03 09:03:35 +0200

    Importation premier composant

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs/Observable';
+import * as Query from '../../global-query';
+import 'rxjs/add/operator/map';
+
+
+
+
 
 @Component({
   selector: 'ngx-dashboard',
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
+  users: Array<any> = []; // List of Users
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    // this.getUsers();
+  }
+
+  getUsers() {
+    this.apollo.watchQuery({ query: Query.Users })
+      .valueChanges
+      .map((result: any) => result.data.users).subscribe((data) => {
+        this.users = data;
+      })
+  }
+
 }
+
+
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
-
+import { NgxEchartsModule } from 'ngx-echarts';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
+import { EchartsPieComponent } from '../../components/echarts/echarts-pie.component';
 
 @NgModule({
   imports: [
     ThemeModule,
+    NgxEchartsModule
   ],
   declarations: [
-    DashboardComponent,
-  ],
+    DashboardComponent, EchartsPieComponent  ],
 })
 export class DashboardModule { }
