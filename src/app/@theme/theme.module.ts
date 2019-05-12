bb62fb16835f4b7d32c910ce70024abf7d709d1e
Alexandre Bouillet <alexandre.bouillet14@gmail.com>
2019-05-12 10:57:03 +0200

    fix auth button

--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -64,6 +64,7 @@ import {
 import { DEFAULT_THEME } from './styles/theme.default';
 import { COSMIC_THEME } from './styles/theme.cosmic';
 import { CORPORATE_THEME } from './styles/theme.corporate';
+import {RouterModule} from "@angular/router";
 
 const BASE_MODULES = [CommonModule, FormsModule, ReactiveFormsModule];
 
@@ -143,7 +144,7 @@ const NB_THEME_PROVIDERS = [
 ];
 
 @NgModule({
-  imports: [...BASE_MODULES, ...NB_MODULES],
+  imports: [...BASE_MODULES, ...NB_MODULES, RouterModule],
   exports: [...BASE_MODULES, ...NB_MODULES, ...COMPONENTS, ...PIPES],
   declarations: [...COMPONENTS, ...PIPES],
   entryComponents: [...ENTRY_COMPONENTS],
